1c1b5e6fdefb665a9c50915c6da09907f60a8982
Gerard Shaughnessy <igpshaughnessy@gmail.com>
2022-06-18 18:44:02 -0500

    before mobile ready2

--- components/views/Gallery.js
+++ components/views/Gallery.js
@@ -9,14 +9,14 @@ import img8 from "../../Assets/img/71270006.jpg";
 import img9 from "../../Assets/img/DSC_0106.jpg";
 import img10 from "../../Assets/img/DSC_0515.jpg";
 import img11 from "../../Assets/img/DSC_0168.jpg";
-import img12 from "../../";
-import img13 from "../../";
-import img14 from "../../";
-import img15 from "../../";
-import img16 from "../../";
-import img17 from "../../";
-import img18 from "../../";
-import img19 from "../../";
+import img12 from "../../Assets/img2/DSC_2440.jpg";
+import img13 from "../../Assets/img2/DSC_3592.jpg";
+import img14 from "../../Assets/img2/DSC_3682.jpg";
+import img15 from "../../Assets/img2/DSC_3740.jpg";
+import img16 from "../../Assets/img2/DSC_4307-2.jpg";
+import img17 from "../../Assets/img2/DSC_4704-3.jpg";
+import img18 from "../../Assets/img2/DSC_4741.jpg";
+import img19 from "../../Assets/img2/P1010035.jpg";
 
 export default () => html`
   <section id="galleryPhotos">
